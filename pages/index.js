ec1575407ede601e11dda02f774a9e360670e096
hirokyammt <hirokyammt@gmail.com>
2021-10-24 23:15:16 +0900

    Add meta tags on top page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import Head from 'next/head'
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
+import { HOME_DESCRIPTION, HOME_URL, SITE_NAME, HOME_IMAGE_URL } from '../lib/constants'
 
 export default function Index({ allPosts }) {
   const heroPost = allPosts[0]
@@ -14,6 +15,13 @@ export default function Index({ allPosts }) {
       <Layout>
         <Head>
           <title>Hiroom.</title>
+          <meta name="description" content={HOME_DESCRIPTION} />
+          <meta property="og:url" content={HOME_URL} />
+          <meta property="og:type" content="blog" />
+          <meta property="og:title" content={SITE_NAME} />
+          <meta property="og:site_name" content={SITE_NAME} />
+          <meta property="og:description" content={HOME_DESCRIPTION} />
+          <meta property="og:image" content={`${HOME_URL}${HOME_IMAGE_URL}`} />
         </Head>
         <Container>
           <Intro />
