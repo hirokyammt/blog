a2ac042b9dda3ae973985245428c992f2925c6e0
hirokyammt <hirokyammt@gmail.com>
2021-10-25 17:19:41 +0900

    Update cover image size to 1200 x 630

--- components/post-header.js
+++ components/post-header.js
@@ -7,7 +7,7 @@ export default function PostHeader({ title, coverImage, date, author }) {
     <>
       <PostTitle>{title}</PostTitle>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} height={620} width={1240} />
+        <CoverImage title={title} src={coverImage} height={630} width={1200} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="mb-6 text-lg">
