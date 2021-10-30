9b05fc2543bebcf29f9eb5ba40ee94bed7408b5a
hirokyammt <hirokyammt@gmail.com>
2021-10-31 01:14:24 +0900

    Update copy link button design

--- components/copy-link.js
+++ components/copy-link.js
@@ -1,13 +1,13 @@
-import { FaLink } from 'react-icons/fa'
+import { ImLink } from 'react-icons/im'
 import copyToClipboard from '../lib/copyToClipboard'
 import { HOME_URL } from '../lib/constants'
 
 export default function CopyLink({ slug }) {
   return (
       <buttun onClick={() => copyToClipboard(`${HOME_URL}/posts/${slug}`)}>
-        <div className="p-4 flex justify-center items-center rounded-lg border border-accent-2 hover:opacity-80 my-8">
-          <FaLink />
-          Copy the URL to share this article!
+        <div className="py-4 flex justify-center items-center border border-accent-2 cursor-pointer hover:opacity-80 my-8">
+          <ImLink size={20} />
+          <span className="pl-2 tracking-wide">Click to Copy Link</span>
         </div>
       </buttun>
   )
