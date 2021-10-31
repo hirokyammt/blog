ad3eb382c50a846d6e417d342e1b0adb652dc7db
hirokyammt <hirokyammt@gmail.com>
2021-10-31 14:04:48 +0900

    Update tags and copy link buttons to rounded

--- components/copy-link.js
+++ components/copy-link.js
@@ -5,7 +5,7 @@ import { HOME_URL } from '../lib/constants'
 export default function CopyLink({ slug }) {
   return (
       <buttun onClick={() => copyToClipboard(`${HOME_URL}/posts/${slug}`)}>
-        <div className="py-4 flex justify-center items-center border border-accent-2 cursor-pointer hover:opacity-80 my-8">
+        <div className="py-4 flex justify-center items-center rounded border border-accent-2 cursor-pointer hover:opacity-80 my-8">
           <ImLink size={20} />
           <span className="pl-2 tracking-wide">Click to Copy Link</span>
         </div>
