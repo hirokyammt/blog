c2761e54019f9ddd70f18f07c43d0dedb2f5e962
hirokyammt <hirokyammt@gmail.com>
2021-11-06 17:39:41 +0900

    Make codes better to fix warnings

--- components/post-header.js
+++ components/post-header.js
@@ -9,10 +9,8 @@ export default function PostHeader({ title, coverImage, date }) {
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} height={630} width={1200} />
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
+      <div className="max-w-2xl text-lg mx-auto mb-6">
+        <DateFormatter dateString={date} />
       </div>
     </>
   )
