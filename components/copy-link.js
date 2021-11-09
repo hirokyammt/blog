62cc4723db7f2409ca3c60ebe6d327d28bec2426
hirokyammt <hirokyammt@gmail.com>
2021-11-09 20:40:39 +0900

    Fix error of copy to clipboard on firefox

--- components/copy-link.js
+++ components/copy-link.js
@@ -4,7 +4,7 @@ import { HOME_URL } from '../lib/constants'
 
 export default function CopyLink({ slug }) {
   return (
-      <div onClick={() => copyToClipboard(`${HOME_URL}/posts/${slug}`)}>
+      <div onClick={(event) => copyToClipboard(event, `${HOME_URL}/posts/${slug}`)}>
         <span className="py-4 flex justify-center items-center rounded border border-accent-2 cursor-pointer hover:opacity-80 my-8">
           <ImLink size={20} />
           <span className="pl-2 tracking-wide">Click to Copy Link</span>
