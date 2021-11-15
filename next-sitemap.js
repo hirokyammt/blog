9f8eca8f4f1d1625d3a0e582fbcf1e9bf93aab11
hirokyammt <hirokyammt@gmail.com>
2021-11-15 20:06:21 +0900

    Import home url from constants file

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,6 @@
+import { HOME_URL } from './lib/constants'
+
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://hiroomblog.com',
+  siteUrl: process.env.SITE_URL || {HOME_URL},
   generateRobotsTxt: true,
 }
