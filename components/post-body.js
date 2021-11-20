492f9413686e7d4bcb18b5f5d1eaaf11be2e089c
hirokyammt <hirokyammt@gmail.com>
2021-11-20 14:14:37 +0900

    Update text size of publish date

--- components/post-body.js
+++ components/post-body.js
@@ -4,7 +4,7 @@ import markdownStyles from '../components/markdown-styles.module.css'
 export default function PostBody({ date, content }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <div className="mb-6">
+      <div className="text-lg mb-6">
         <DateFormatter dateString={date} />
       </div>
       <div
