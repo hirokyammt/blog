f87c6ec1a6b1175310d22e2db12a6dd027a21afc
hirokyammt <hirokyammt@gmail.com>
2021-12-11 02:00:30 +0900

    Add search icon on top page

--- components/intro.js
+++ components/intro.js
@@ -1,9 +1,14 @@
+import { FaSearch } from 'react-icons/fa'
+
 export default function Intro() {
   return (
-    <section className="lg:px-12 flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="lg:px-12 flex items-center justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight">
         Hiroom.
       </h1>
+      <div className="text-left text-xl md:text-3xl">
+        <FaSearch />
+      </div>
     </section>
   )
 }
