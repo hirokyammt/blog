c197723f3c43c4918ccb2ab1d05b3fd94cada6e8
hirokyammt <hirokyammt@gmail.com>
2021-12-11 19:15:57 +0900

    Update z index of logo on search window

--- components/intro.js
+++ components/intro.js
@@ -7,7 +7,7 @@ export default function Intro() {
 
   return (
     <section className="lg:px-12 flex items-center justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight">
+      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight z-20">
         Hiroom.
       </h1>
       <div
