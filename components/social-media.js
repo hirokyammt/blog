e02756d048a679190ec1a093edb4b285b2549469
hirokyammt <hirokyammt@gmail.com>
2022-01-22 14:50:28 +0900

    Fix a bug to open links in new tab

--- components/social-media.js
+++ components/social-media.js
@@ -4,18 +4,18 @@ import { FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'
 export default function SocialMedia() {
   return (
     <>
-      <Link target="_blank" href="https://twitter.com/hirokyammt">
-        <a className="hover:opacity-80 mx-4">
+      <Link href="https://twitter.com/hirokyammt">
+        <a target="_blank" className="hover:opacity-80 mx-4">
           <FaTwitter title={'Twitter'} />
         </a>
       </Link>
-      <Link target="_blank" href="https://instagram.com/hirokyammt">
-        <a className="hover:opacity-80 mx-4">
+      <Link href="https://instagram.com/hirokyammt">
+        <a target="_blank" className="hover:opacity-80 mx-4">
           <FaInstagram title={'Instagram'} />
         </a>
       </Link>
-      <Link target="_blank" href="https://linkedin.com/in/hirokyammt">
-        <a className="hover:opacity-80 mx-4">
+      <Link href="https://linkedin.com/in/hirokyammt">
+        <a target="_blank" className="hover:opacity-80 mx-4">
           <FaLinkedin title={'LinkedIn'} />
         </a>
       </Link>
