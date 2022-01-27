6e9b714baa5527b20b4d5dceb888c1ce457c91c9
hirokyammt <hirokyammt@gmail.com>
2022-01-27 22:53:37 +0900

    Add tagline on top page

--- components/intro.js
+++ components/intro.js
@@ -1,9 +1,12 @@
 export default function Intro() {
   return (
-    <section className="lg:px-12 flex items-center justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight">
+    <section className="lg:px-12 flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-7xl md:text-8xl font-bold tracking-tighter leading-tight">
         Hiroom.
       </h1>
+      <p className="text-center md:text-left text-lg font-bold mt-5 md:pl-8">
+        Insights for product management
+      </p>
     </section>
   )
 }
