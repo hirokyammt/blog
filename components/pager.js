b1241d9954dfbad2df1d20ed0b27624f576dd0d1
hirokyammt <hirokyammt@gmail.com>
2022-02-19 20:45:28 +0900

    Capitalize title of archive page

--- components/pager.js
+++ components/pager.js
@@ -7,7 +7,7 @@ export default function Pager({ id, posts, totalIds }) {
   return (
     <section className="lg:px-12">
       <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight mt-16 mb-16 md:mb-12">
-        All posts
+        All Posts
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-24 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
