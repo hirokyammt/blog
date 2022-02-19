910a3e7b0ba8959c8f2fffdd9aa00022f23d03f5
hirokyammt <hirokyammt@gmail.com>
2022-02-19 14:41:16 +0900

    Refactor category logic

--- pages/categories/[tag].js
+++ pages/categories/[tag].js
@@ -9,7 +9,7 @@ import Layout from '../../components/layout'
 import { getCategorizedPosts, getAllTags } from '../../lib/api'
 import { HOME_DESCRIPTION, HOME_URL, SITE_NAME, HOME_IMAGE_URL } from '../../lib/constants'
 
-export default function Category({ posts, category, preview }) {
+export default function Category({ tag, posts, preview }) {
   const router = useRouter()
   if (!router.isFallback && !posts) {
     return <ErrorPage statusCode={404} />
@@ -25,18 +25,18 @@ export default function Category({ posts, category, preview }) {
             <article className="mb-32">
               <Head>
                 <title>
-                  Categorized by {category.replace('-', ' ')} | {SITE_NAME}
+                  Categorized by {tag.replace('-', ' ')} | {SITE_NAME}
                 </title>
                 <meta name="description" content={HOME_DESCRIPTION} />
-                <meta property="og:url" content={`${HOME_URL}/categories/${category}`} />
+                <meta property="og:url" content={`${HOME_URL}/categories/${tag}`} />
                 <meta property="og:type" content="article" />
-                <meta property="og:title" content={`Categorized by ${category.replace('-', ' ')} | ${SITE_NAME}`} />
+                <meta property="og:title" content={`Categorized by ${tag.replace('-', ' ')} | ${SITE_NAME}`} />
                 <meta property="og:site_name" content={SITE_NAME} />
                 <meta property="og:description" content={HOME_DESCRIPTION} />
                 <meta property="og:image" content={`${HOME_URL}${HOME_IMAGE_URL}`} />
               </Head>
               {posts && (
-                <CategorizedPosts category={category} posts={posts} />
+                <CategorizedPosts tag={tag} posts={posts} />
               )}
             </article>
           </>
@@ -51,16 +51,16 @@ export async function getStaticProps({ params }) {
     'title',
     'date',
     'slug',
+    'tags',
     'coverImage',
     'excerpt',
-    'tags',
   ])
-  const category = params.tag
+  const tag = params.tag
 
   return {
     props: {
       posts,
-      category
+      tag
     },
   }
 }
