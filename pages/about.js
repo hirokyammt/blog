6c9a6454a923224aef9c14d9b0a870f72d7e0837
hirokyammt <hirokyammt@gmail.com>
2022-04-26 00:58:23 +0900

    Update about me

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Container from '../components/container'
 import Header from '../components/header'
 import Layout from '../components/layout'
 import SocialMedia from '../components/social-media'
+import CoverImage from '../components/cover-image'
 import BackToHome from '../components/back-to-home'
 import { HOME_URL, SITE_NAME, HOME_IMAGE_URL, ABOUT_DESCRIPTION } from '../lib/constants'
 
@@ -23,38 +23,46 @@ export default function AboutMe({ preview }) {
           <meta property="og:description" content={ABOUT_DESCRIPTION} />
           <meta property="og:image" content={HOME_IMAGE_URL} />               
         </Head>
-        <div className="h-screen flex items-center bg-accent-1 border-b border-accent-2">
-          <Container>
+        <div className="h-screen flex items-center justify-center bg-accent-1 border-b border-accent-2">
+          <section className="mb-8">
             <Header />
-            <section className="lg:px-12 mb-8">
-              <div className="text-lg lg:text-xl">
-                <p>Hello! I am Hiroki,</p>
-                <p className="py-2 text-3xl lg:text-4xl font-bold">Product Manager</p>
-                <p>focus on software engineering and data science.</p>
-                <p className="py-4">Please have a seat and scroll down.</p>
-                <div className="pt-20 text-3xl lg:text-4xl flex duration-200">
-                  <SocialMedia />
-                </div>
+            <div className="lg:px-12 text-lg md:text-xl">
+              <p>Hello! I am Hiroki,</p>
+              <p className="py-2 text-3xl md:text-4xl font-bold">Product Manager</p>
+              <p>focus on software engineering and data science.</p>
+              <p className="py-4">Please have a seat and scroll down.</p>
+              <div className="pt-20 text-3xl md:text-4xl flex duration-200">
+                <SocialMedia />
               </div>
-            </section>
-          </Container>
+            </div>
+          </section>
         </div>
-        <div className="max-w-2xl text-lg leading-relaxed mx-auto px-5">  
-          <h1 className="text-4xl md:text-5xl font-bold tracking-tighter leading-tight md:leading-none mt-12 mb-8">
-            About me
-          </h1>
-          <p>
-            I am a product manager at the largest e-commerce company in Japan.
-            I have an ability of software development as well as am getting better in marketing and design fields through working with cross-functional teams.
-            I am currently working on product management, project management and sometimes data science.
-          </p>
-          <p>
-            This is a personal blog that stores my experience as a product manager.
-            The website itself is also developed by myself with Next.js and Markdown.
-            If you are interested in the source code, you can find the Github link below.
-          </p>
-          <BackToHome />
-        </div>
+        <Container>
+          <div className="max-w-2xl text-lg leading-relaxed mx-auto">  
+            <h1 className="text-4xl md:text-5xl font-bold tracking-tighter leading-tight md:leading-none mt-12 mb-8">
+              About me
+            </h1>
+            <div className="mb-8">
+              <p>
+                I am a product manager at the largest e-commerce company in Japan.
+                I have an ability of software development as well as am getting better in marketing and design fields through working with cross-functional teams.
+                I am currently working on product management, project management and sometimes data science.
+              </p>
+              <p>
+                This is a personal blog that stores my experience as a product manager.
+                The website itself is also developed by myself with Next.js and Markdown.
+                If you are interested in the source code, you can find the Github link below.
+              </p>
+            </div>
+            <CoverImage
+              title={"cover image"}
+              src={HOME_IMAGE_URL}
+              height={630}
+              width={1200}
+            />
+            <BackToHome />
+          </div>
+        </Container>
       </article>  
     </Layout>
   )
